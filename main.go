accc19119ec3089fcbd43d0a055122b86b3d645e
liyou-chen <liyou1202@gmail.com>
2024-05-02 07:37:25 +0800

    Max profit

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"leetcodePractice/arr"
-)
-
 func main() {
-	nums1 := []int{1, 2, 3, 4, 5, 6, 7}
-	k := 3
-	arr.Rotate(nums1, k)
-	fmt.Print(nums1)
+	nums1 := []int{7, 1, 5, 3, 6, 4}
+	println(maxProfit(nums1))
+}
+
+func maxProfit(prices []int) int {
+	profit := 0
+	buy := prices[0]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < buy {
+			buy = prices[i]
+			continue
+		}
+		temp := prices[i] - buy
+		if profit > 0 {
+			profit += temp
+			buy = prices[i]
+		}
+	}
+	return profit
 }
